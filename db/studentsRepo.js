f2e3994cc461b87e7ace66e2bcd5ce9391a9c5b7
mosheyz <mosheyz770@gmail.com>
2026-07-28 17:15:33 +0300

    create sessions repo

--- db/studentsRepo.js
+++ db/studentsRepo.js
@@ -3,26 +3,23 @@ import { students } from '../db/students.js';
 import { ObjectId } from 'mongodb';
 
 
-const studentsRepo = {
-  create: async (student) => {
-    try {
+const studentsRepo = (students) => {
+  return {
+    create: async (student) => {
+        const result = await students.insertOne(student)
+        return result.insertedId.toString()
+    },
 
-      const result = await students.insertOne(student)
-      return result.insertedId.toString()
-    } catch (error) {
-      console.error(error)
-    }
-  },
+    findById: async (id) => {
+      const student = await students.findOne({ _id: new ObjectId(id) })
+      return student
+    },
 
-  findById: async (id) => {
-    const student = await students.findOne({ _id: new ObjectId(id) })
-    return student
-  },
-
-  update: async (studentId, labId) => {
-    const result = await students.updateOne(
-      { _id: new ObjectId(studentId) },
-      { $addToSet: {labSessionsIds: labId} })
+    update: async (studentId, labId) => {
+      const result = await students.updateOne(
+        { _id: new ObjectId(studentId) },
+        { $addToSet: { labSessionsIds: labId } })
       return result.modifiedCount;
+    }
   }
 }
